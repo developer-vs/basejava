ef18a439e4067b8e74c629ab552fe79eb9a2bc2d
Vladimir Semenovich <vladimirgdsr@gmail.com>
2020-03-02 13:06:42 -0800

    rebase project

---
+++ src/com/webapp/model/Resume.java
@@ -0,0 +1,35 @@
+package com.webapp.model;
+
+import java.util.Objects;
+
+public class Resume {
+
+    // Unique identifier
+    private String uuid;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    @Override
+    public String toString() {
+        return uuid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Resume)) return false;
+        Resume resume = (Resume) o;
+        return getUuid().equals(resume.getUuid());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUuid());
+    }
+}
